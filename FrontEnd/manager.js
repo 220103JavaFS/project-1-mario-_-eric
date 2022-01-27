55b2956d42afee959ce334fa827a74e910b656dd
Eric Florence <ericflorence.dev@gmail.com>
2022-01-26 20:35:33 -0700

    fix sum js

--- FrontEnd/manager.js
+++ FrontEnd/manager.js
@@ -1,32 +1,36 @@
 function menuDropdown() {
+
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-var div = document.getElementsByClassName('dropdown-content');
+var div = document.getElementsByClassName("dropdown-content");
 
-for(var  i =0;i<div.length;i++){
+for(var i = 0; i < div.length; i++){
 
-    for(var  j =0;j<div[i].children.length;j++){
+    for(var j = 0; j < div[i].children.length; j++){
 
-        div[i].children[j].addEventListener('click',function(){
+        div[i].children[j].addEventListener("click", function(){
 
             this.parentNode.previousElementSibling.innerHTML = this.innerHTML;
         })
     }
 }
 
-
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!event.target.matches(".dropbtn")) {
+    
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    
+    for (i = 0; i < dropdowns.length; i++) {
+      
+      var openDropdown = dropdowns[i];
+      
+      if (openDropdown.classList.contains("show")) {
+          openDropdown.classList.remove("show");
       }
     }
   }
+}
