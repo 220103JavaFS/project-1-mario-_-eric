8ee9886f2d9fa541d4619d77ae869a174f4d0afa
unknown <57272096+MarioJimenezZ@users.noreply.github.com>
2022-02-02 18:20:56 -0800

    Fixed ports

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -6,8 +6,6 @@ import io.javalin.http.staticfiles.Location;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Timestamp;
-
 public class App {
 
     private static Javalin app;
